f30b63576591f08aa1573bb26f41723b58834c1b
Valery Emelyanov <emelva@rambler.ru>
2019-10-20 12:19:22 +0400

    Fixing security

--- src/main/java/com/example/shopapp/configs/SecurityConfiguration.java
+++ src/main/java/com/example/shopapp/configs/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/products/delete/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and().formLogin().loginPage("/login").permitAll()
                 .loginProcessingUrl("/authenticateTheUser");
